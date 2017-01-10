725d7a1e6c7e4e1e021d515be29957900353f641
marianmorcotet <marian.m177@yahoo.com>
2017-01-10 17:32:27 +0200

    Add files via upload
    in sf ceva merge

--- flappy/header.h
+++ flappy/header.h
@@ -3,7 +3,6 @@
 #include <iostream>
 struct teava
 {
-    int viteza=4;
     int pozitieX;
     int yDeSus;
 };
@@ -12,6 +11,8 @@ void updatePasare(int &y, float &velocity);
 void updateTeava(int pozX);
 void afisareTeava(int pozX, int pozY);
 void afisareMeniu();
+void verificaDacaMaiFaciOTeava(int &nrtevi,unsigned int &framecount,teava tevi[]);
+void verificareDacaPrimaTeavaAIesitDePeEcran(int &nrtevi, teava tevi[]);
 int height=400;
 int lenght=300;
 int pasareAxaX=90;
@@ -19,4 +20,5 @@ float gravity=3.4;
 int lift=-23;
 float velocity=0;
 unsigned int framecount=0;
+int vitezaTevi=4;
 teava tevi[2];
--- flappy/main.cpp
+++ flappy/main.cpp
@@ -3,25 +3,24 @@ void afisareMeniu()
 {
 
 }
-void updatePasare(int &y,float &velocity)
+void updatePasare(int &y,float &velocity,bool &caca)
 {
     if(GetAsyncKeyState( VK_UP ) & 0x8000 )velocity+=lift;
     velocity+=gravity;
     velocity*=0.6;
     y+=int(velocity);
     afisarePasare(y);
-    if(height-y<=0)velocity=0;
-    if(y>height)velocity=height;
+    if(y<=0)caca=0;
+    if(y>height)caca=0;
 }
 
 void afisareTeava(int pozX, int pozY)
 {
     bar(pozX,0,pozX+25,pozY);//25 grosimea tevii
-    bar(pozX,pozY+60,pozX+25,height);//60 distanta intre tevi
+    bar(pozX,pozY+70,pozX+25,height);//60 distanta intre tevi
 }
 void afisarePasare(int y)
 {
-     cleardevice();
      setfillstyle(SOLID_FILL,YELLOW);
      bar(pasareAxaX,y-10,pasareAxaX+20,y+5);
      setfillstyle(SOLID_FILL,LIGHTRED);
@@ -29,31 +28,40 @@ void afisarePasare(int y)
      setfillstyle(SOLID_FILL,BLUE);
      bar(pasareAxaX+15,y-5,pasareAxaX+18,y-8);
 }
+
   int main()
   {
       initwindow(lenght,height);
-      int nrtevi=0;
       int pozY=height/2;
-      bool ok=1;
-      while(ok)
+      bool pasareaEsteInViata=1;
+      tevi[0].pozitieX=300;
+      tevi[0].yDeSus=rand()%361+40;
+      tevi[1].pozitieX=500;
+      tevi[1].yDeSus=rand()%361+40;
+      tevi[2].pozitieX=700;
+      tevi[2].yDeSus=rand()%361+40;
+      while(pasareaEsteInViata)
       {
-          if(nrtevi<3&&framecount==100)
-          {
-              tevi[nrtevi].pozitieX=300;
-              tevi[nrtevi].yDeSus=rand()%381+20;
-              nrtevi++;
-              framecount-=100;
-          }
+          //verificareDacaPrimaTeavaAIesitDePeEcran(nrtevi,tevi[]);
           if(tevi[0].pozitieX<=-24)
           {
               for(int i=0;i<2;i++)
               {
                   tevi[i]=tevi[i+1];
               }
+              tevi[2].pozitieX=tevi[1].pozitieX+200;
+              tevi[2].yDeSus=rand()%361+40;
           }
-          updatePasare(pozY,velocity);
+          std::cout<<framecount<<" ";
+          for(int j=0;j<=2;j++)
+          {
+              tevi[j].pozitieX-=vitezaTevi;
+              afisareTeava(tevi[j].pozitieX,tevi[j].yDeSus);
+          }
+          updatePasare(pozY,velocity,pasareaEsteInViata);
           framecount++;
           delay(35);
-      }
+          cleardevice();
+        }
       return 0;
   }
