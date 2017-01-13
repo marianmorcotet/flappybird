1028ee40f87c7f5a637a4082b3713fb9a8ac7a1a
marianmorcotet <marian.m177@yahoo.com>
2017-01-13 15:59:49 +0200

    Add files via upload
    IT'S ALIVE!!!!!!

--- flappy/main.cpp
+++ flappy/main.cpp
@@ -1,27 +1,55 @@
 #include "header.h"
+void afisareBackground()
+{
+    setfillstyle(SOLID_FILL,LIGHTGREEN);
+    bar(0,268,300,400);
+    setfillstyle(SOLID_FILL,LIGHTBLUE);
+    bar(0,0,300,268);
+}
+void tipNor0(int x, int y)
+{
+    setfillstyle(SOLID_FILL,CYAN);
+    bar(x,y,x+40,y+30);
+    bar(x+20,y,x+27,y-7);
+    bar(x-7,y+7,x+7,y+14);
+}
+void tipNor1(int x, int y)
+{
+    setfillstyle(SOLID_FILL,CYAN);
+    bar(x,y,x+30,y+20);
+    bar(x+27,y+17,x+34,y+24);
+}
+void tipNor2(int x, int y)
+{
+
+}
 void ecranSfarsitJoc()
 {
     settextstyle(GOTHIC_FONT,0,2);
     outtextxy(70,120,"AI PIERDUT");
     settextstyle(GOTHIC_FONT,0,1);
-    outtextxy(36,160,"Apasa orice tasta");
-    outtextxy(52,180,"pentru a reveni.");
+    outtextxy(32,160,"Apasa tasta escape");
+    outtextxy(62,180,"pentru a reveni.");
 }
 void navigareMeniu(int &selectie,int &pozitieSelector)
 {
     afisareMeniu();
-    afisarePasareMaiMica(pozitieSelect);
-    if(GetAsyncKeyState( VK_UP ) & 0x8000)
+    afisarePasareMaiMica(pozitieSelector);
+    if(GetAsyncKeyState( VK_DOWN ) & 0x8000)
     {
         if(pozitieSelector==95)pozitieSelector=115;
           else pozitieSelector=135;
     }
-    if(GetAsyncKeyState(VK_DOWN) & 0x8000)
+    if(GetAsyncKeyState(VK_UP) & 0x8000)
     {
         if(pozitieSelector==135)pozitieSelector=115;
           else pozitieSelector=95;
     }
-    delay(40);
+    if(GetAsyncKeyState(VK_SPACE) & 0x8000)
+        if(pozitieSelector==95)selectie=1;
+           else if(pozitieSelector==115)selectie=2;
+                   else selectie=3;
+    delay(70);
     cleardevice();
 
 }
@@ -33,20 +61,25 @@ void afisareMeniu()
     outtextxy(24,90,"1.Start joc");
     outtextxy(24,110,"2.Instructiuni");
     outtextxy(24,130,"3.Iesi");
+    settextstyle(DEFAULT_FONT,0,2);
+    outtextxy(10,150,"Apasa sus/jos pentru");
+    outtextxy(34,170,"a naviga si space");
+    outtextxy(39,190,"pentru a selecta.");
+
 }
 void updatePasare(int &y,float &velocity,bool &caca)
 {
-    if(GetAsyncKeyState( VK_UP ) & 0x8000)velocity+=lift;
+    if(GetKeyState( VK_SPACE ) & 0x8000)velocity+=lift;
     velocity+=gravity;
     velocity*=0.6;
     y+=int(velocity);
     afisarePasare(y);
     if(y-10<=0)caca=0;
     if(y+5>height)caca=0;
+    //check coliziune
     if((pasareAxaX>tevi[0].pozitieX-5 || pasareAxaX+20>tevi[0].pozitieX)&&(pasareAxaX<tevi[0].pozitieX+30 || pasareAxaX+20<tevi[0].pozitieX+30))
         if(y-10<tevi[0].yDeSus || y+5>tevi[0].yDeSus+70)caca=0;
 }
-
 void afisareTeava(int pozX, int pozY)
 {
     setfillstyle(SOLID_FILL,GREEN);
@@ -77,28 +110,69 @@ void afisarePasare(int y)
   int main()
   {
       initwindow(lenght,height);
-      start:
-      int selectie=0;
-      int pozY=height/2;
-      bool pasareaEsteInViata=1;
-      srand(time(0));
+      meniu:
+      srand(time(0));//seed random pt ca obstacolele sa fie cu adevarat random
+      //calculeaza pozitia primelor 3 obstacole
       tevi[0].pozitieX=300;
       tevi[0].yDeSus=rand()%201+100;
       tevi[1].pozitieX=500;
       tevi[1].yDeSus=rand()%201+100;
       tevi[2].pozitieX=700;
       tevi[2].yDeSus=rand()%201+100;
-      std::cout<<tevi[0].yDeSus<<" "<<tevi[1].yDeSus<<" "<<tevi[2].yDeSus<<" ";
-      int pozitieSelect=95;
+      nori[0].pozitieX=100;
+      nori[0].pozitieY=rand()%101;
+      nori[0].viteza=rand()%6;
+      nori[0].tip=rand()%4;
+      nori[1].pozitieX=200;
+      nori[1].pozitieY=rand()%101;;
+      nori[1].viteza=rand()%6;
+      nori[1].tip=rand()%4;
+      nori[2].pozitieX=300;
+      nori[2].pozitieY=rand()%101;
+      nori[2].viteza=rand()%6;
+      nori[2].tip=rand()%4;
+      nori[3].pozitieX=400;
+      nori[3].pozitieY=rand()%101;
+      nori[3].viteza=rand()%6;
+      nori[3].tip=rand()%4;
       while(selectie==0)
       {
-          navigareMeniu(selectie,pozitieSelect);
+          navigareMeniu(selectie,pozitieSelector);
       }
-      if(selectie==1){
+      if(selectie==1)goto startjoc;
+         else if(selectie==2)goto instructiuni;
+                 else goto iesire;
+      instructiuni:
+      //afisareInstructiuni();
+
+
+      startjoc:
+          score=0;
       while(pasareaEsteInViata)
       {
+          afisareBackground();
+          //nori
+          for(int j=0;j<4;j++)
+          {
+              if(nor[j].pozitieX<=-30)
+              {
+                for(int k=j;k<3;k++)
+                {
+                    nori[k]=nori[k+1];
+                }
+                nori[3].pozitieX=400;
+                nori[3].pozitieY=rand()%101;
+                nori[3].viteza=rand()%6;
+                nori[3].tip=rand()%4;
+              }
+              if(nori[j].tip==0)tipNor0(nori[j].pozitieX,nori[j].pozitieY);
+                 else if(nori[j].tip==1)tipNor1(nori[j].pozitieX,nori[j].pozitieY);
+                         else if(nori[j].tip==2)tipNor2(nori[j].pozitieX,nori[j].pozitieY);
+                                 else tipNor3(nori[j].pozitieX,nori[j].pozitieY);
+              nori[j].pozitieX-=nori[j].viteza;
+          }
           //verificareDacaPrimaTeavaAIesitDePeEcran(nrtevi,tevi[]);
-          if(tevi[0].pozitieX<=-24)
+          if(tevi[0].pozitieX<=-30)
           {
               for(int i=0;i<2;i++)
               {
@@ -106,7 +180,6 @@ void afisarePasare(int y)
               }
               tevi[2].pozitieX=tevi[1].pozitieX+200;
               tevi[2].yDeSus=rand()%201+100;
-              std::cout<<tevi[2].yDeSus<<" ";
           }
           for(int j=0;j<=2;j++)
           {
@@ -114,11 +187,27 @@ void afisarePasare(int y)
               afisareTeava(tevi[j].pozitieX,tevi[j].yDeSus);
           }
           updatePasare(pozY,velocity,pasareaEsteInViata);
-          framecount++;
           delay(intarziere);
           cleardevice();
         }
-        }
-
+      gameover:
+          revenireLaMeniu=0;
+      while(revenireLaMeniu==0)
+      {
+          ecranSfarsitJoc();
+          delay(40);
+          cleardevice();
+          if((GetAsyncKeyState(VK_ESCAPE) & 0x8000))
+          {
+              selectie=0;
+              pasareaEsteInViata=1;
+              revenireLaMeniu=1;
+              cleardevice();
+              pozY=90;
+              velocity=0;
+          }
+      }
+      goto meniu;
+      iesire:
       return 0;
   }
